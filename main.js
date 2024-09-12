c5d8ebf86c538d79885c4f716dee0bca334061c7
John Trehearn <johntrehearnbc@gmail.com>
2024-09-12 11:48:09 +0300

    working prototype

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const __todosList = document.getElementById('todos');
 
 const __todosJson = JSON.parse(localStorage.getItem('todos_items'));
 
-if(__todosJson){
+if (__todosJson) {
     __todosJson.forEach(todo => {
         addTask(todo);
     });
@@ -13,33 +13,77 @@ if(__todosJson){
 
 document.body.addEventListener('keyup', (e) => {
     e.preventDefault();
-    if(e.key == 'Enter'){
+    if (e.key == 'Enter') {
         addTask();
     }
 });
-
 
 __btnAdd.addEventListener('click', (e) => {
     e.preventDefault();
     addTask();
 });
 
-
-function addTask(todo){
-    console.log("TASK IS ADDED")
+function addTask(todo) {
+    console.log("TASK IS ADDED");
     let todoText = __newTask.value;
 
-   if(todo){
-       todoText = todo.text;
+    if (todo) {
+        todoText = todo.text;
     }
 
-    if(todoText){
+    if (todoText) {
         const element = document.createElement('li');
+        const span = document.createElement('span');
+        const buttonUpdate = document.createElement('button');
+        const buttonDelete = document.createElement('button');
 
-        if (todo && todo.completed){
+        if (todo && todo.completed) {
             element.classList.add('completed');
         }
-     
+
+        span.innerText = todoText;
+
+        element.appendChild(span);
+
+        buttonDelete.innerHTML = '<em class="fa fa-trash"></em>';
+        buttonDelete.className = 'btn btn-lg btn-danger';
+
+        buttonDelete.addEventListener('click', () => {
+            element.remove();
+            update();
+        });
+
+        element.appendChild(buttonDelete);
+
+        buttonUpdate.innerHTML = '<em class="fa fa-check"></em>';
+        buttonUpdate.className = 'btn btn-lg btn-primary';
+
+        buttonUpdate.addEventListener('click', () => {
+            element.classList.toggle('completed');
+            update();
+        });
+
+        element.appendChild(buttonUpdate);
+
+        __todosList.appendChild(element);
+
+        __newTask.value = '';
+
+        update();
     }
 
+    function update() {
+        const __elements = document.querySelectorAll('li');
+
+        const __todos = [];
+
+        __elements.forEach(element => {
+            __todos.push({
+                text: element.querySelector('span').innerText,
+                completed: element.classList.contains('completed')
+            });
+        });
+
+        localStorage.setItem('todos_items', JSON.stringify(__todos));
+    }
 }
