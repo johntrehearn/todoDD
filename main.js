47ec17ff270856c56998f6bbc4b4f285734f3bbc
John Trehearn <johntrehearnbc@gmail.com>
2024-09-12 10:09:53 +0300

    variables added

---
+++ main.js
@@ -0,0 +1,4 @@
+const __form = document.getElementById('form');
+const __btnAdd = document.getElementById('btn-add');
+const __newTask = document.getElementById('newtask');
+const __todosList = document.getElementById('todos');
